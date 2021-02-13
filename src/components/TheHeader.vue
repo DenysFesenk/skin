c17c08c7ceeb355bc15267fc9ca69f2df0e9a1f0
Denys Fesenko <dfesuk@gmail.com>
2021-02-14 01:09:32 +0200

    :tada: start new project

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="header">
+    <a href="#" class="header__logo">
+      <img src="../assets/Union.png" alt="" />
+    </a>
+    <div class="header__content">
+        <h1 class="header__title">Do you love your skin? </h1>
+        <span class="header__subtitle">PM loves your skin!</span>
+        <div class="header__box">
+            <the-button class="header__btn">Order now </the-button>
+            <a href="/" class="header__link">Dermatologist consultation</a>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import TheButton from './common/TheButton.vue';
+export default {
+    name: 'The-header',
+    components: { TheButton },
+};
+</script>
+
+<style lang="scss">
+.header {
+  min-height: 100vh;
+  @include flex(center, center, column);
+  position: relative;
+  background-image: url('../assets/Render Passes.png');
+  background-repeat:  no-repeat;
+  background-color: $pink-color;
+  width: 100%;
+  &__logo{
+      position: absolute;
+      top: 57px;
+      left: 50%;
+      transform: translateX(-50%);
+  }
+  &__title{
+      @include text($H1200, 900, $dark-color){
+          max-width: 654px;
+          text-align: left;
+          line-height: 113px;
+          letter-spacing: 1px;
+          text-transform: capitalize;
+      };
+      margin-bottom: 12px;
+  }
+  &__subtitle{
+      @include text($H240, 900, $dark-color){
+          text-align: center;
+          line-height: 27px;
+          letter-spacing: 1px;
+      };
+      display: block;
+      margin-bottom: 200px;
+  }
+  &__btn{
+    margin-right: 50px;
+  }
+}
+</style>
---
+++ src/components/common/TheButton.vue
@@ -0,0 +1,26 @@
+<template>
+  <button class="btn">
+      <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+    name: 'The-button',
+}
+</script>
+
+<style lang="scss">
+    .btn{
+        background-image: url('../../assets/Union (1).png');
+        @include text($H240, normal, $pink-color){
+            line-height: 28px;
+            text-align: center;
+            letter-spacing: 0.5px;
+            text-transform: capitalize;
+        };
+        outline: none;
+        border: none;
+        cursor: pointer;
+    }
+</style>
